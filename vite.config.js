105303eea801d7742f6c14a28461ca6e098d66c2
Christer Carlsson <ccarlsson@outlook.com>
2026-03-04 10:59:32 +0100

    fix: update base path configuration in vite.config.js for GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Pages_test/',
+  base: process.env.GITHUB_ACTIONS ? '/Pages_test/' : '/',
   plugins: [vue()],
 })
 
